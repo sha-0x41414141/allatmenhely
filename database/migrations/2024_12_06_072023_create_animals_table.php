54f18b1b680fbe76b888ff4d4e18cc9dae2c2aa6
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2024-12-06 08:31:16 +0100

    db

---
+++ database/migrations/2024_12_06_072023_create_animals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('animal_name');
+            $table->boolean('gender');
+            $table->integer('age');
+            $table->unsignedBigInteger('shelter_id');
+            $table->unsignedBigInteger('breed_id');
+            $table->foreign('breed_id')->references('id')->on('breeds')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
